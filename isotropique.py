4a0ae979755a9e52b8072e3d0dea07ba7123fe90
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-01-29 19:14:52 +0100

    Update isotropique.py

--- isotropique.py
+++ isotropique.py
@@ -1,4 +1,84 @@
 from grid_printer import GridPrinter
+from random import random, randint
+from time import sleep
+from os import get_terminal_size
+from os import system
+from copy import deepcopy
+from time import time
 
+
+class Cell:
+    def __init__(self, alive: bool, speed: tuple[int] =(0, 0)):
+        self.alive = bool(alive)
+        self.speed = (speed[0], speed[1])
+
+    def new_position(self, x: int, y: int) -> tuple[int]:
+        if not self.alive:
+            return (x, y)
+        return (x + self.speed[0], y + self.speed[1])
+
+    def __str__(self):
+        return "██" if self.alive else "  "
+
+    def __repr__(self):
+        return f"Cell({self.alive}, {self.speed})"
+
+def random_bool(probability: float =.5):
+    return random() < float(probability)
+
+
+def random_grid(width: int,
+                height: int,
+                probability: float =0.5) -> list[list[Cell]]:
+    grid = [
+            [
+                Cell(random_bool(probability),
+                     speed=(randint(-1, 2), randint(-1, 2)))
+                for _ in range(width)]
+            for _ in range(height)]
+    return grid
+
+def aff_grid(grid: list[list[Cell]]) -> None:
+    system("clear")
+    for line in grid:
+        for cell in line:
+            print(cell, end="")
+        print()
+
+def update_cell(x: int, y: int, grid: list[list[Cell]]) -> list[list[Cell]]:
+    new_y, new_x = (grid[y][x]).new_position(y, x)
+    if new_y >= len(grid):
+        return grid
+    if new_x >= len(grid[0]):
+        return grid
+    # print(x, y, "|", new_x, new_y)
+    grid[new_y][new_x] = grid[y][x]
+    grid[y][x] = Cell(False)
+    return grid
+
+def next_grid(grid: list[list[Cell]]) -> list[list[Cell]]:
+    return update_cell(randint(0, len(grid[0])-1), randint(0, len(grid)-1), grid)
+    # new_grid = grid
+    # for y, line in enumerate(grid):
+    #     for x, cell in enumerate(line):
+    #         new_grid = update_cell(x, y, grid)
+    # return new_grid
+
+def main():
+    width, height = get_terminal_size()
+    width, height = width, height//2
+    grid = random_grid(width, height, probability=.1)
+    while True:
+        previous_time = time()
+        aff_grid(grid)
+        while time() - previous_time < 0.05:
+            grid = next_grid(grid)
+        # input()
+
+
+
+
+if __name__ == "__main__":
+    main()
 
 
